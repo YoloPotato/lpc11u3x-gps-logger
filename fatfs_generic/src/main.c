46dab308b2ee0905e3ee6e3ff8bcd8472b302a24
olikraus <olikraus@gmail.com>
2016-08-28 08:46:43 +0200

    fatfs generic

---
+++ fatfs_generic/src/main.c
@@ -0,0 +1,100 @@
+/*
+  main.c
+  
+  Two function must be implemented:
+    void __attribute__ ((interrupt)) SysTick_Handler(void)
+    int __attribute__ ((noinline)) main(void)
+    
+    
+*/
+
+#include <chip.h>
+#include "delay.h"
+#include "u8g2.h"
+
+
+/*=======================================================================*/
+/* u8x8_lpc11u3x.c */
+uint8_t u8x8_gpio_and_delay_lpc11u3x(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+
+/*=======================================================================*/
+/* little_rook_chess.c */
+void chess_Init(u8g2_t *u8g, uint8_t body_color);
+void chess_exec(void) ;
+
+
+/*=======================================================================*/
+u8g2_t u8g2;
+
+/*=======================================================================*/
+/* system procedures and sys tick master task */
+
+#define SYS_TICK_PERIOD_IN_MS 100
+
+
+volatile uint32_t sys_tick_irq_cnt=0;
+
+
+void __attribute__ ((interrupt)) SysTick_Handler(void)
+{
+  sys_tick_irq_cnt++;
+}
+
+
+
+/*=======================================================================*/
+/*
+  setup the hardware and start interrupts.
+  called by "Reset_Handler"
+*/
+int __attribute__ ((noinline)) main(void)
+{
+
+  /* call to the lpc lib setup procedure. This will set the IRC as clk src and main clk to 48 MHz */
+  /* it will also enable IOCON, see sysinit_11xx.c */
+  Chip_SystemInit(); 
+
+  /* if the clock or PLL has been changed, also update the global variable SystemCoreClock */
+  /* see chip_11xx.c */
+  SystemCoreClockUpdate();
+  
+  /* set systick and start systick interrupt */
+  SysTick_Config(SystemCoreClock/1000UL*(unsigned long)SYS_TICK_PERIOD_IN_MS);
+  
+  /* turn on GPIO */
+  Chip_GPIO_Init(LPC_GPIO);
+  
+  /* turn on IOCON... this is also done in Chip_SystemInit() */
+  Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_IOCON);
+  
+  Chip_GPIO_SetPinDIROutput(LPC_GPIO, 0, 7);	/* port 0, pin 7: LED on eHaJo Breakout Board */
+
+  //delay_micro_seconds(10UL*1000UL);
+  
+  u8g2_Setup_ssd1306_i2c_128x64_noname_1(&u8g2, U8G2_R0, u8x8_byte_sw_i2c, u8x8_gpio_and_delay_lpc11u3x);
+  u8g2_InitDisplay(&u8g2);
+  u8g2_SetPowerSave(&u8g2, 0);
+  chess_Init(&u8g2, 1);  /* assuming OLED here, so make the body_color be 1 for the white OLED pixel */
+  for(;;)
+    chess_exec();
+
+
+  
+  for(;;)
+  {
+    Chip_GPIO_SetPinOutHigh(LPC_GPIO, 0, 7);
+    delay_micro_seconds(500UL*1000UL);
+    
+    Chip_GPIO_SetPinOutLow(LPC_GPIO, 0, 7);    
+    delay_micro_seconds(500UL*1000UL);
+  }
+  
+  
+  /* enter sleep mode: Reduce from 1.4mA to 0.8mA with 12MHz */  
+  while (1)
+  {
+    SCB->SCR |= (1UL << SCB_SCR_SLEEPONEXIT_Pos);		/* enter sleep mode after interrupt */ 
+    Chip_PMU_SleepState(LPC_PMU);						/* enter sleep mode now */
+  }
+}
+
