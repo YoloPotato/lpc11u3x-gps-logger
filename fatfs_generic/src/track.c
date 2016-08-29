7a81ee5bc35356e84fb428123f363796e14c5454
olikraus <olikraus@gmail.com>
2016-08-29 22:47:26 +0200

    track structure

--- fatfs_generic/src/track.c
+++ fatfs_generic/src/track.c
@@ -2,7 +2,37 @@
 
   track.c
   
-  high level interface:
-    write_track_point(...)
+TP.time
+In: TP, Threshold [s]
 
+if gpx file does not exist then
+   create gpx file
+   write header to gpx file
+   write trackstart to gpx file
+   write trackend to gpx file
+   write footer to gpx file
+   lasttime = tp.time
+   last time eeprom init
+else
+  lasttime = get last time from eeprom
+end if
+
+if tp.time - lasttime > Threshold then
+  seek to end of track in gpx file
+  write trackend to gpx file
+  write trackstart to gpx file
+  write trackend to gpx file
+  write footer to gpx file
+end if
+
+seek to end of track in gpx file
+write trackpoint to gpx file
+write trackend to gpx file
+write footer to gpx file
+store last time to eeprom
+
+====
+
+eeprom:
+init, get last time, store new time
 */
